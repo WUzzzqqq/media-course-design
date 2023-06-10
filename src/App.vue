aed1062630a72743394946a25d6ad16f74c878b3
15957 <1595774144@qq.com>
2023-06-10 19:45:18 +0800

    创建

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="a">
+  <button @click="sub">sss</button>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      msg:'Hello World!',
+      a:""
+    }
+  },
+  methods:{
+    sub(){
+      console.log(this.a);
+    }
+  }
+}
+</script>
