7a9751d7cc3a2631c31da10916995a6330f4558d
SSwater <307722647@qq.com>
2023-06-10 22:35:37 +0800

    加入vue-b-m(error)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <el-input v-model="msg"></el-input><br>
   <el-button @click="pop">测试</el-button>
+  <baidu-map :center="center" :zoom="zoom" @ready="handler"></baidu-map>
 </template>
 
+<style>
+.bm-view {
+  width: 100%;
+  height: 300px;
+}
+</style>
 <script>
 
 import {ElMessage} from "element-plus";
@@ -11,7 +18,9 @@ export default{
 
   data(){
     return{
-      msg:'ss'
+      msg:'ss',
+      center: {lng: 0, lat: 0},
+      zoom: 3
     }
   },
   methods:{
@@ -21,6 +30,12 @@ export default{
         message: '你好,'+this.msg,
         type: 'info'
       })
+    },
+    handler ({BMap, map}) {
+      console.log(BMap, map)
+      this.center.lng = 116.404
+      this.center.lat = 39.915
+      this.zoom = 15
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './assets/main.css'
 
+import BaiduMap from 'vue-baidu-map'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-const app = createApp(App)
+let app = createApp(App)
 
 app.use(ElementPlus)
 app.mount('#app')
+
+app.use(BaiduMap, {
+    ak: '1YLpdgUFuoXtxMQsiFlpn9G1BYdM1mM9'
+})
